bbda26d54527d8fde19327eb0ff29ecc5db58f7d
Tore Gjerdrum <tore.gjerdrum@epinova.no>
2019-04-24 16:08:43 +0200

    Idempotent role assignment

--- src/role/Assignment.fs
+++ src/role/Assignment.fs
@@ -11,9 +11,29 @@ module Assignment =
           value: string
         }
 
+    let private exists assigneeObjectId role scope = 
+        let result = azResult (sprintf "role assignment list --assignee %s --role %s --scope %s" assigneeObjectId role scope)
+        
+        match result.Result.Error, result.ExitCode, result.Result.Output with
+            | (_, 0, x) when (x = "[]") ->
+                false
+            | _, 0, x ->
+                true
+            | error, exitCode, _ ->
+                failwithf "Failed to list role assignement. ExitCode %d. Message: %s" exitCode error
+
     let create assigneeObjectId role s =
         let buildScope = 
             sprintf "/subscriptions/%s/resourceGroups/%s/providers/%s/%s/%s" s.subscription s.resourceGroup s.provider s.key s.value
-
-        az (sprintf "role assignment create --assignee-object-id %s --role %s --scope %s" assigneeObjectId role buildScope)
+    
+        if exists assigneeObjectId role buildScope then
+            ""
+        else
+            let result = azResult (sprintf "role assignment create --assignee-object-id %s --role %s --scope %s" assigneeObjectId role buildScope)
+    
+            match result.Result.Error, result.ExitCode with
+                | _, 0 ->                
+                    result.Result.Output.Trim().Trim('"')            
+                | error, exitCode ->
+                    failwithf "Possibly transient error. ExitCode %d. Message: %s" exitCode error
     
