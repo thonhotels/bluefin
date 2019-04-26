dd1d92661b1b92896620c60310acb4ef9ed57ecb
Olav Nybo <olavnybo@gmail.com>
2019-04-26 15:58:24 +0200

    create WebApp

---
+++ src/webapp/App.fs
@@ -0,0 +1,34 @@
+namespace Bluefin.Webapp
+
+open Bluefin.Core
+open Newtonsoft.Json
+
+module App =
+    type Tag = {
+        key: string
+        value: string
+    }
+    type WebApp = { 
+        resourceGroup: string   // Name of resource group
+        name: string            // Name of the new web app
+        plan: string            // Name or resource id of the app service plan
+        planResourceGroup: string option // Set this if app service plan is not in the same resource group as the web app
+                                // on the form: /subscriptions/<subscriptionId>/resourceGroups/<resourcegroup name>
+        tags: seq<Tag>          // tags as key/value
+    }
+
+    let create a =
+        let tagToString s =
+            if System.String.IsNullOrEmpty s.value then s.key
+            else sprintf "%s=%s" s.key s.value
+        
+        let planIdOrName name rg =
+            match rg with
+            | Some x -> sprintf "%s/providers/Microsoft.Web/serverfarms/%s" x name
+            | None -> name
+
+        let tagArg =
+            if Seq.isEmpty a.tags then ""
+            else "--tags " + (a.tags |> Seq.map tagToString |> String.concat " ")
+        az (sprintf "webapp create -g %s -p %s -n %s %s" a.resourceGroup (planIdOrName a.plan a.planResourceGroup) a.name tagArg) |> ignore 
+        az (sprintf "webapp update -g %s -n %s --https-only true" a.resourceGroup a.name)      
