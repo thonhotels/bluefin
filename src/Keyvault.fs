115c612c4a510b4fd814756671f1f3020350f514
Olav Nybo <olavnybo@gmail.com>
2019-05-04 18:00:44 +0200

    setSecret

--- src/Keyvault.fs
+++ src/Keyvault.fs
@@ -5,3 +5,6 @@ open Bluefin.Core
 module Keyvault =
     let getSecret vaultName name =
         az (sprintf "keyvault secret show --vault-name %s --name %s --query value" vaultName name)
+
+    let setSecret vaultName name value = 
+        az (sprintf "keyvault secret set --vault-name %s --name %s --value %s" vaultName name value) |> ignore    
