57fde2f79371b48f58d1ca9d17b7432104596761
Olav Nybo <olavnybo@gmail.com>
2019-05-04 18:17:02 +0200

    redact secret value

--- src/Keyvault.fs
+++ src/Keyvault.fs
@@ -7,4 +7,7 @@ module Keyvault =
         az (sprintf "keyvault secret show --vault-name %s --name %s --query value" vaultName name)
 
     let setSecret vaultName name value = 
-        az (sprintf "keyvault secret set --vault-name %s --name %s --value %s" vaultName name value) |> ignore    
+        azRedact 
+            (sprintf "keyvault secret set --vault-name %s --name %s --value %s" vaultName name value)
+            (sprintf "keyvault secret set --vault-name %s --name %s --value %s" vaultName name "****")
+            |> ignore    
