e56fb398e2c44ba4b87539c5dd89414da8d6bc02
Olav Nybo <olavnybo@gmail.com>
2019-07-02 16:32:11 +0200

    adapt to non-idempotent create webapp in az 2.067 hope to revert this change when az is idempotent again

--- src/webapp/App.fs
+++ src/webapp/App.fs
@@ -2,6 +2,8 @@ namespace Bluefin.Webapp
 
 open Bluefin.Core
 open Config
+open Bluefin.Http
+open System.Net
 
 module App =
     type Tag = {
@@ -30,7 +32,44 @@ module App =
         ipSecurityRestrictions = [||]
     }
 
-    let create a =
+    type SiteProperties = {
+        httpsOnly: bool
+        outboundIpAddresses: string
+        serverFarmId: string
+    }
+
+    type Site = {
+        id: string
+        kind: string
+        location: string
+        name: string
+        properties: SiteProperties
+    }
+
+    let get rg name =
+        let url = 
+            sprintf "resourceGroups/%s/providers/Microsoft.Web/sites/%s?api-version=2016-08-01" rg name
+
+        let accessTokenResult = getAccessToken "https://management.azure.com"
+
+        let result = get<Site> url (Some accessTokenResult.accessToken)
+        match (result) with
+               |(HttpStatusCode.OK, site) -> Some site
+               |(HttpStatusCode.NotFound, _) -> None
+               |(statusCode, value) -> failwithf "Could not get web app. Status code is %A. Content: %A" statusCode value
+
+               
+    let private createWebApp rg plan planResourceGroup name tags =
+        
+        let siteCompatible site =
+            let planId =
+                let pRg = match planResourceGroup with 
+                            | Some x -> x
+                            | None -> rg
+                sprintf "%s/providers/Microsoft.Web/serverfarms/%s" pRg plan
+            site.properties.serverFarmId = planId
+
+
         let tagToString s =
             if System.String.IsNullOrEmpty s.value then s.key
             else sprintf "%s=%s" s.key s.value
@@ -41,9 +80,17 @@ module App =
             | None -> name
 
         let tagArg =
-            if Seq.isEmpty a.tags then ""
-            else "--tags " + (a.tags |> Seq.map tagToString |> String.concat " ")
-        az (sprintf "webapp create -g %s -p %s -n %s %s" a.resourceGroup (planIdOrName a.plan a.planResourceGroup) a.name tagArg) |> ignore 
+            if Seq.isEmpty tags then ""
+            else "--tags " + (tags |> Seq.map tagToString |> String.concat " ")
+
+        let existing = get rg name
+        match existing with
+        | Some site -> if (not (siteCompatible site)) then failwithf "Cant create web app. Site already exist with incompatible settings"
+        | None -> az (sprintf "webapp create -g %s -p %s -n %s %s" rg (planIdOrName plan planResourceGroup) name tagArg) |> ignore
+        
+
+    let create a =
+        createWebApp a.resourceGroup a.plan a.planResourceGroup a.name a.tags
         az (sprintf "webapp update -g %s -n %s --https-only true" a.resourceGroup a.name) |> ignore
         az (sprintf "webapp config set -g %s -n %s --always-on %b --remote-debugging-enabled false" a.resourceGroup a.name a.alwaysOn) |> ignore    
         if not (Seq.isEmpty a.ipSecurityRestrictions) then 
