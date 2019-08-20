a3b4ab1d07747e380e2496b723fae88e742d889d
Olav Nybo <olavnybo@gmail.com>
2019-08-20 15:24:13 +0200

    typed deserialization of identity

--- src/Identity.fs
+++ src/Identity.fs
@@ -1,8 +1,9 @@
 namespace Bluefin
 
+open System.Net
+open Newtonsoft.Json
 open Bluefin.Core
 open Bluefin.Http
-open System.Net
 
 module Identity =
     type LocationType = {
@@ -24,17 +25,37 @@ module Identity =
                     value
                |(statusCode, value) -> failwithf "Could not create managed identity (user assigned). Status code is %A. Content: %s" statusCode value
 
+    type Tag = {
+        key: string
+        value: string
+    }
+    type Properties = {
+        clientId: string
+        clientSecretUrl: string
+        principalId: string
+        tenantId: string
+    }
+    type IdentityResponse = {
+        id: string
+        location: string
+        name: string
+        properties: Properties
+        tags: System.Collections.Generic.Dictionary<string,string>
+        [<JsonProperty("type")>]
+        _type: string
+    }
+
     let get rg name =
         let url = 
-            sprintf "resourceGroups/%s/providers/Microsoft.ManagedIdentity/userAssignedIdentities/%s?api-version=2015-08-31-preview" rg name
+            sprintf "resourceGroups/%s/providers/Microsoft.ManagedIdentity/userAssignedIdentities/%s?api-version=2018-11-30" rg name
 
         let accessTokenResult = getAccessToken "https://management.azure.com"
 
-        let result = get Management url (Some accessTokenResult.accessToken)
+        let result = get<IdentityResponse> Management url (Some accessTokenResult.accessToken)
         match (result) with
                |(HttpStatusCode.OK, value) -> 
                     value
-               |(statusCode, value) -> failwithf "Could not get managed identity (user assigned). Status code is %A. Content: %s" statusCode value
+               |(statusCode, value) -> failwithf "Could not get managed identity (user assigned). Status code is %A. Content: %A" statusCode value
 
     let resourceId rg name = 
         (Group.rgId rg) + (sprintf "/providers/Microsoft.ManagedIdentity/userAssignedIdentities/%s" name)    
