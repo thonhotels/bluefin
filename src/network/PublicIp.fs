91da5cf8fdd7c09a1d4826b95e63ae9c639ccdf2
Olav Nybo <olavnybo@gmail.com>
2019-08-20 13:49:21 +0200

    implemented get Public IP Address

--- src/network/PublicIp.fs
+++ src/network/PublicIp.fs
@@ -1,6 +1,9 @@
 namespace Bluefin.Network
 
+open System.Net
 open Bluefin.Core
+open Bluefin.Http
+open Newtonsoft.Json
 
 module PublicIp =
     type AllocationMethod = Dynamic | Static
@@ -9,10 +12,10 @@ module PublicIp =
     let createAdvanced rg name (allocationMethod:AllocationMethod) dnsName (sku:Sku) =
         let dnsSegment = 
             match dnsName with
-            | Some n -> sprintf "--dns-name %s" n
-            | None -> ""
+            | Some n -> ["--dns-name";n]
+            | None -> []
 
-        az (sprintf "network public-ip create -g %s --n %s --allocation-method %s --sku %s %s" rg name (allocationMethod.ToString()) (sku.ToString()) dnsSegment)
+        azArr <| ["network";"public-ip";"create";"-g";rg;"-n";name;"--allocation-method";allocationMethod.ToString();"--sku";sku.ToString()] @ dnsSegment
         
     let createBasic rg name (allocationMethod:AllocationMethod) = 
         createAdvanced rg name allocationMethod None Sku.Basic
@@ -22,3 +25,44 @@ module PublicIp =
    
     let resourceId rg name =
         Common.resourceId rg "publicIPAddresses" name        
+
+    type DnsSettings = {
+        domainNameLabel: string
+        fqdn: string
+    }
+
+    type SkuType = {
+        name: Sku
+        tier: string
+    }
+    type IPAddressVersion = IPv4 | IPv6
+    type ProvisioningState =  Succeeded | Updating | Deleting | Failed
+    type IpAddressProperties = {
+        provisioningState: string
+        resourceGuid: string
+        ipAddress: string
+        publicIPAddressVersion: string
+        publicIPAllocationMethod: string        
+        idleTimeoutInMinutes: int
+        dnsSettings: DnsSettings
+    }
+    type IpAddressResponse = {
+        name: string
+        id: string
+        etag: string
+        location: string
+        [<JsonProperty("type")>]
+        _type: string
+        properties: IpAddressProperties
+        sku: SkuType
+    }
+    let get rg name =
+        let url = 
+            sprintf "resourceGroups/%s/providers/Microsoft.Network/publicIPAddresses/%s?api-version=2017-04-01" rg name
+
+        let accessTokenResult = getAccessToken "https://management.azure.com"
+
+        let result = get<IpAddressResponse> Management url (Some accessTokenResult.accessToken)
+        match (result) with
+               |(HttpStatusCode.OK, value) -> value
+               |(statusCode, value) -> (failwithf "Could not get public ip address. Status code is %A. Content: %A" statusCode value)
