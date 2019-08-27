5de82c0118eec6dd9ea8ecf5864e9880da3c9b11
Olav Nybo <olavnybo@gmail.com>
2019-08-27 12:01:24 +0200

    create assigment made idempotent

--- src/role/Assignment.fs
+++ src/role/Assignment.fs
@@ -6,16 +6,28 @@ open Bluefin.Http
 open Newtonsoft.Json
 
 module Assignment =
-    let private exists assigneeObjectId role scope = 
+    type RoleAssignment = {
+        id: string
+        name: string
+        principalId: string
+        principalName: string
+        roleDefinitionId: string
+        roleDefinitionName: string
+        scope: string
+        [<JsonProperty("type")>]
+        _type: string
+    }
+
+    let private get assigneeObjectId role scope =
         let result = azResult (sprintf "role assignment list --assignee %s --role %s --scope %s" assigneeObjectId role scope)
         
         match result.Result.Error, result.ExitCode, result.Result.Output with
             | (_, 0, x) when (x = "[]") ->
-                false
+                None
             | _, 0, x ->
-                true
+                Some (Seq.head <| JsonConvert.DeserializeObject<RoleAssignment[]> x)
             | error, exitCode, _ ->
-                failwithf "Failed to list role assignement. ExitCode %d. Message: %s" exitCode error
+                failwithf "Failed to list role assignement. ExitCode %d. Message: %s" exitCode error    
 
     type AssignRoleProperties = {
         roleDefinitionId: string
@@ -44,15 +56,16 @@ module Assignment =
         createdBy: string
         updatedBy: string
     }
-    type RoleAssigment = {
+    type CreateRoleAssigment = {
         id: string
         [<JsonProperty("type")>]
         _type: string
         name: string
         properties: RoleAssignmentProperties
     }
+
     type CreateAssignmentResult = 
-    | RA of RoleAssigment
+    | RA of RoleAssignment
     | Code of BadRequests 
 
     let create assigneeObjectId role (scope:string) =
@@ -62,25 +75,40 @@ module Assignment =
             | v when v.error.code = "PrincipalNotFound" -> Code PrincipalNotFound
             | _ -> Code Unknown
 
-        let s = scope.Replace(sprintf "/subscription/%s/" subscriptionId,"") 
-        let url = 
-            sprintf "%s/providers/Microsoft.Authorization/roleAssignments/%s?api-version=2018-01-01-preview" s (Guid.NewGuid().ToString())
+        let toRoleAssignment (ra:CreateRoleAssigment):RoleAssignment = {
+            id = ra.id
+            name = ra.name
+            principalId = ra.properties.principalId
+            principalName = ""
+            roleDefinitionId = ra.properties.roleDefinitionId
+            roleDefinitionName = ""
+            scope = ra.properties.scope
+            _type = ra._type
+        }
 
-        let accessTokenResult = getAccessToken "https://management.azure.com"
+        match get assigneeObjectId role scope with
+        | Some roleAssignment -> RA roleAssignment
+        | _ ->
+            let s = scope.Replace(sprintf "/subscription/%s/" subscriptionId,"") 
+            let url = 
+                sprintf "%s/providers/Microsoft.Authorization/roleAssignments/%s?api-version=2018-01-01-preview" s (Guid.NewGuid().ToString())
 
-        let result = put Management url (Some accessTokenResult.accessToken) 
-                        (Some (box {properties = {
-                            roleDefinitionId = role
-                            principalId = assigneeObjectId
-                        }
-                        }))
-        match (result) with
-               |(Net.HttpStatusCode.Created, value)|(Net.HttpStatusCode.OK, value) ->
-                    RA <| JsonConvert.DeserializeObject<RoleAssigment> value
-               |(Net.HttpStatusCode.BadRequest, value) -> decodeBadRequest value
-               |(statusCode, value) -> failwithf "Could not create roleAssignments. Status code is %A. Content: %s" statusCode value
+            let accessTokenResult = getAccessToken "https://management.azure.com"
+
+            let result = put Management url (Some accessTokenResult.accessToken) 
+                            (Some (box {properties = {
+                                roleDefinitionId = role
+                                principalId = assigneeObjectId
+                            }
+                            }))
+            match (result) with
+                   |(Net.HttpStatusCode.Created, value)|(Net.HttpStatusCode.OK, value) ->
+                        RA (toRoleAssignment <| JsonConvert.DeserializeObject<CreateRoleAssigment> value)
+                   |(Net.HttpStatusCode.BadRequest, value) -> decodeBadRequest value
+                   |(statusCode, value) -> failwithf "Could not create roleAssignments. Status code is %A. Content: %s" statusCode value
     
     let createWithRetry objId role (scope:string) =
+
         let rec attemptCall n =
             if n < 36 then 
                 debugfn "Trying to find Service principal with objectId = %s. %d attempt" objId n
