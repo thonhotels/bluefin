09980c7350dca97a52222e55e138e8f5c3d33c27
Kjell Breimo <kjellbr@gmail.com>
2019-09-25 15:12:36 +0200

    Add appConfig create

--- src/AppConfig.fs
+++ src/AppConfig.fs
@@ -1,6 +1,7 @@
 namespace Bluefin
 
 open System
+open System.Net
 open Bluefin.Core
 open Bluefin.Http
 open Newtonsoft.Json
@@ -31,3 +32,39 @@ module AppConfig =
 
     let getFirstConnectionString resourceGroup name =
         List.head <| getConnectionStrings resourceGroup name
+    
+    type LocationType = {
+        location: string
+    }
+
+    type Properties = {
+        provisioningState: string
+        creationDate: string
+        endpoint: string
+    }
+
+    type ConfigurationStore = {
+        [<JsonProperty("type")>]
+        _type: string
+        properties: Properties
+        id: string
+        name: string
+        location: string
+    }
+
+    let create rg name =
+        let url = sprintf "https://management.azure.com/subscriptions/%s/resourceGroups/%s/providers/Microsoft.AppConfiguration/configurationStores/%s?api-version=2019-02-01-preview" subscriptionId rg name
+        let accessTokenResult = getAccessToken "https://management.azure.com"
+
+        let result = put Management url (Some accessTokenResult.accessToken) (Some (box {location = defaultLocation}))
+        match (result) with
+               |(HttpStatusCode.OK, value) -> 
+                    printfn "Updated appconfig"
+                    JsonConvert.DeserializeObject<ConfigurationStore> value
+               |(HttpStatusCode.Created, value) ->
+                    printfn "Created appconfig"
+                    JsonConvert.DeserializeObject<ConfigurationStore> value
+               |(statusCode, value) -> failwithf "Could not create appconfig %s %s. Status code is %A. Content: %s" rg name statusCode value
+
+
+    
