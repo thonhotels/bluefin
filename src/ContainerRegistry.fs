a6565191f99720904228d315a30595dfbbd85baa
Olav Nybo <olavnybo@gmail.com>
2019-09-28 16:46:34 +0200

    add acr login

--- src/ContainerRegistry.fs
+++ src/ContainerRegistry.fs
@@ -1,5 +1,15 @@
 namespace Bluefin
 
+open Core
+
 module ContainerRegistry =
     let resourceId rg name =
         (Group.rgId rg) + (sprintf "/providers/Microsoft.ContainerRegistry/registries/%s" name)
+    
+    let login name =
+        azArr [
+            "acr"
+            "login"
+            "--name"
+            name
+        ] |> ignore
