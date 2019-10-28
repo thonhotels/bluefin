d2b7e4498b8b6f7ebf508e4a486ba718d57263c5
Karl Solgård <karl.f91@gmail.com>
2019-10-28 10:02:08 +0100

    Failing time job tests fixed

--- tests/unitTests/JobsTests.fs
+++ tests/unitTests/JobsTests.fs
@@ -1,17 +1,16 @@
 module JobsTests
 
 open Xunit
-open System
 open Bluefin
 
 [<Fact>]
 let ``My test`` () =
     //
-    let r = Jobs.buildFilterString Jobs.IntervalType.Minute 10
+    let r = Jobs.buildIntervalFilterString Jobs.TimeUnit.Minute 10
     Assert.Equal("(sys.Label = 'Time.Events.MinuteHasPassed') AND (((Minute % 10) = 0))", r)
 
 [<Fact>]
 let ``My test 55`` () =
     //
-    let r = Jobs.buildFilterString Jobs.IntervalType.Minute 55
+    let r = Jobs.buildIntervalFilterString Jobs.TimeUnit.Minute 55
     Assert.Equal("(sys.Label = 'Time.Events.MinuteHasPassed') AND (Minute > 0 AND ((Minute % 55) = 0))", r)
