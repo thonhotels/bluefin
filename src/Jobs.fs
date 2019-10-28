ab4da08b88c6820f6317b115c2ffe2796876b1b1
Olav Nybo <olavnybo@gmail.com>
2019-10-28 09:32:25 +0100

    added support for scheduling jobs on specific time

--- src/Jobs.fs
+++ src/Jobs.fs
@@ -10,21 +10,27 @@ open Bluefin.Servicebus
 open System
 
 module Jobs =
-    type IntervalType = Minute | Hour | Day | Month | Year
+    type TimeUnit = Minute | Hour | Day | Month | Year 
+    
+    type IntervalType = string * TimeUnit * int * TimeSpan
 
-    let buildFilterString intervalType interval = 
-        let typeToLabel = 
-          match intervalType with
-          | Minute -> "(sys.Label = 'Time.Events.MinuteHasPassed')"
-          | Hour -> "(sys.Label = 'Time.Events.HourHasPassed')"
-          | Day -> "(sys.Label = 'Time.Events.DayHasPassed')"
-          | Month -> "(sys.Label = 'Time.Events.MonthHasPassed')"
-          | Year -> "(sys.Label = 'Time.Events.YearHasPassed')"
+    type SpecificTimeType = string * TimeUnit * int * TimeSpan * TimeSpan
 
+    type Job = Interval of IntervalType | Specific of SpecificTimeType
+
+    let typeToLabel = 
+      function 
+      | Minute -> "(sys.Label = 'Time.Events.MinuteHasPassed')"
+      | Hour -> "(sys.Label = 'Time.Events.HourHasPassed')"        
+      | Day -> "(sys.Label = 'Time.Events.DayHasPassed')"
+      | Month -> "(sys.Label = 'Time.Events.MonthHasPassed')"
+      | Year -> "(sys.Label = 'Time.Events.YearHasPassed')"
+
+    let buildIntervalFilterString timeUnit interval = 
         let buildIntervalFilter interval = 
           if interval <= 0 then failwithf "interval must be a positive number"
           let propName = 
-            match intervalType with
+            match timeUnit with
             | Minute -> "Minute"
             | Hour -> "Hour"
             | Day -> "Day"
@@ -35,27 +41,18 @@ module Jobs =
             | Minute when 60 % interval <> 0 -> "Minute > 0 AND "
             | _ -> ""    
 
-          sprintf "(%s((%s %% %d) = 0))" (runAt0 intervalType) propName interval
+          sprintf "(%s((%s %% %d) = 0))" (runAt0 timeUnit) propName interval
+      
+        if interval = 0 then typeToLabel timeUnit else (sprintf "%s AND %s" (typeToLabel timeUnit)  (buildIntervalFilter interval))
 
-        if interval = 0 then typeToLabel else (sprintf "%s AND %s" typeToLabel (buildIntervalFilter interval))
+    let buildSpecificTimeFilterString timeUnit time =
+      let filterString =
+        sprintf "%s = %d" (timeUnit.ToString()) time
+      sprintf "%s AND %s" (typeToLabel timeUnit) filterString
 
-    let createJobSubscriptions rg servicebusNamespace serviceName jobs =
+    let createJobSubscriptions rg servicebusNamespace serviceName (jobs:seq<Job>) =
 
-      let createTimeSubscription subscriptionName intervalType interval lockDuration =
-        let divideTimespan (t:TimeSpan) divisor = 
-          TimeSpan.FromTicks (t.Ticks / divisor)
-
-          
-        let timeToLive = 
-          let timespanInterval = 
-            match intervalType with
-            | Minute -> TimeSpan (0,0,interval,0)           
-            | Hour -> TimeSpan (0,interval,0,0)
-            | Day -> TimeSpan (interval,0,0,0)
-            | Month -> TimeSpan (28,0,0,0)
-            | Year -> TimeSpan (28,0,0,0)
-          min (divideTimespan timespanInterval 2L) (TimeSpan.FromDays 14.0)
-          
+      let createTimeSubscription subscriptionName lockDuration timeToLive =
         Subscription.create
                       rg servicebusNamespace "time.events" subscriptionName 
                       { Subscription.defaultSubscription with
@@ -64,16 +61,39 @@ module Jobs =
                           lockDuration = lockDuration
                       }
       
-      let createRule subscriptionName intervalType interval =
+      let createIntervalRule subscriptionName timeUnit interval =
                     Rule.createComplete 
                             rg servicebusNamespace "time.events" subscriptionName "timerule" 
-                            (Rule.buildSqlRule (buildFilterString intervalType interval))
+                            (Rule.buildSqlRule (buildIntervalFilterString timeUnit interval))
 
-      let createJob jobDescription =
-        let jobName, intervalType, interval, lockDuration = jobDescription
-        let subscriptionName = sprintf "%s.%s" serviceName jobName
-        createTimeSubscription subscriptionName intervalType interval lockDuration
-        createRule subscriptionName intervalType interval              
-        
+      let createSpecificTimeRule subscriptionName timeUnit time =
+                    Rule.createComplete 
+                            rg servicebusNamespace "time.events" subscriptionName "timerule" 
+                            (Rule.buildSqlRule (buildSpecificTimeFilterString timeUnit time))
+
+      let createJob (job:Job) =
+        let intervalTimeToLive tu interval = 
+          let divideTimespan (t:TimeSpan) divisor = 
+            TimeSpan.FromTicks (t.Ticks / divisor)
+
+          let timespanInterval = 
+            match tu with
+            | Minute -> TimeSpan (0,0,interval,0)           
+            | Hour -> TimeSpan (0,interval,0,0)            
+            | Day -> TimeSpan (interval,0,0,0)
+            | Month -> TimeSpan (28,0,0,0)
+            | Year -> TimeSpan (28,0,0,0)
+          min (divideTimespan timespanInterval 2L) (TimeSpan.FromDays 14.0)
+
+        match job with
+          | Interval (jobName, timeUnit, interval, lockDuration) -> 
+            let subscriptionName = sprintf "%s.%s" serviceName jobName
+            createTimeSubscription subscriptionName lockDuration (intervalTimeToLive timeUnit interval)
+            createIntervalRule subscriptionName timeUnit interval
+          | Specific (jobName, timeUnit, time, lockDuration, timeToLive) -> 
+            let subscriptionName = sprintf "%s.%s" serviceName jobName
+            createTimeSubscription subscriptionName lockDuration timeToLive
+            createSpecificTimeRule subscriptionName timeUnit time
+
       jobs |>
-      List.iter createJob 
+      Seq.iter createJob 
