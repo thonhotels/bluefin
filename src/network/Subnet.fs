b9b8caf7ae52e46cc790a4cb62d85aab7940b3c3
Tore Gjerdrum <tore.gjerdrum@epinova.no>
2019-11-26 16:25:44 +0100

    Added funciton for createOrUpdate and changed to azArr instead of az

--- src/network/Subnet.fs
+++ src/network/Subnet.fs
@@ -7,10 +7,16 @@ module Subnet =
         azResult (sprintf "network vnet subnet show -g %s -n %s --vnet-name %s" rg name vnetName)
 
     let update rg name vnetName addressPrefixes =
-        az (sprintf "network vnet subnet update -g %s -n %s --vnet-name %s --address-prefixes %s" rg name vnetName addressPrefixes)
+        azArr ["network";"vnet";"subnet";"update";"-g";rg;"-n";name;"--vnet-name";vnetName;"--address-prefixes";addressPrefixes]
 
     let create rg name vnetName addressPrefixes  = 
-        az (sprintf "network vnet subnet create -g %s -n %s --vnet-name %s --address-prefixes %s" rg name vnetName addressPrefixes)
+        azArr ["network";"vnet";"subnet";"create";"-g";rg;"-n";name;"--vnet-name";vnetName;"--address-prefixes";addressPrefixes]
+
+    let createOrUpdate rg name vnetName addressPrefixes =
+        let subnet = show rg name vnetName
+        match subnet with
+        | Some _ -> update rg name vnetName addressPrefixes
+        | None -> create rg name vnetName addressPrefixes
  
     let resourceId rg vnetName name =
-        Common.resourceId rg "virtualNetworks" vnetName + (sprintf "/subnets/%s" name)      
+        Common.resourceId rg "virtualNetworks" vnetName + (sprintf "/subnets/%s" name)
