0fac4dca9e1839d9ed0e4cd67238aa2542b179e4
Tore Gjerdrum <tore.gjerdrum@epinova.no>
2019-11-26 12:45:15 +0100

    Fixed bug when adding vnet integration to webapp

--- src/webapp/VNetIntegration.fs
+++ src/webapp/VNetIntegration.fs
@@ -16,7 +16,9 @@ module VNetIntegration =
         id: string
         location: string
     }
-    let buildRequest id location subnetResourceId =
+    let buildRequest rg siteName location subnetResourceId =
+        let id = 
+            sprintf "/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Web/sites/%s/config/virtualNetwork" subscriptionId rg siteName
         {id = id;location=location;properties={subnetResourceId=subnetResourceId;swiftSupported="true"}}
 
     type Properties = {
@@ -36,15 +38,11 @@ module VNetIntegration =
     let buildSubnetId vnetRg vnetName subnetName =
         sprintf "/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Network/virtualNetworks/%s/subnets/%s" subscriptionId vnetRg vnetName subnetName
 
-    let buildId rg siteName = 
-        sprintf "/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Web/sites/%s/networkconfig/virtualNetwork" subscriptionId rg siteName
-
-    let buildUrl id =
-        sprintf "https://management.azure.com%s?api-version=2018-11-01" id
+    let buildUrl rg siteName =
+        sprintf "https://management.azure.com/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Web/sites/%s/networkconfig/virtualNetwork?api-version=2018-11-01" subscriptionId rg siteName
 
     let get rg siteName =
-        let id = buildId rg siteName
-        let url = buildUrl id
+        let url = buildUrl rg siteName
         let accessTokenResult = getAccessToken "https://management.azure.com"
 
         let result = get<VNetConnection> Management url (Some accessTokenResult.accessToken) 
@@ -55,11 +53,11 @@ module VNetIntegration =
 
     let add rg siteName vnetRg vnetName subnetName =
         let subnetId = buildSubnetId vnetRg vnetName subnetName
-        let id = buildId rg siteName
-        let url = buildUrl id
+
+        let url = buildUrl rg siteName
         let accessTokenResult = getAccessToken "https://management.azure.com"
 
-        let result = put Management url (Some accessTokenResult.accessToken) (Some (box <| buildRequest id defaultLocation subnetId))
+        let result = put Management url (Some accessTokenResult.accessToken) (Some (box <| buildRequest rg siteName defaultLocation subnetId))
         match (result) with
                |(HttpStatusCode.OK, value) -> 
                     printfn "Updated vnet integration"
@@ -67,4 +65,4 @@ module VNetIntegration =
                |(HttpStatusCode.Created, value) ->
                     printfn "Added vnet integration"
                     JsonConvert.DeserializeObject<VNetConnection> value
-               |(statusCode, value) -> failwithf "Could not create vnet integration %s. Status code is %A. Content: %s" id statusCode value
+               |(statusCode, value) -> failwithf "Could not create vnet integration. Status code is %A. Content: %s" statusCode value
