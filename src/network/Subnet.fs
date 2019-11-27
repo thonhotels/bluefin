989e9834b101dd6b1d47fd45f1d5f2957cf88c6e
Olav Nybo <olavnybo@gmail.com>
2019-11-27 09:53:00 +0100

    createOrUpdateForWeb added

--- src/network/Subnet.fs
+++ src/network/Subnet.fs
@@ -6,18 +6,30 @@ module Subnet =
     let private show rg name vnetName =
         azResult (sprintf "network vnet subnet show -g %s -n %s --vnet-name %s" rg name vnetName)
 
+    let private subnetCreationArguments op rg name vnetName addressPrefixes =
+        [
+            "network";"vnet";"subnet";op
+            "-g";rg;"-n";name;"--vnet-name";vnetName
+            "--address-prefixes";addressPrefixes
+        ]
+
     let update rg name vnetName addressPrefixes =
-        azArr ["network";"vnet";"subnet";"update";"-g";rg;"-n";name;"--vnet-name";vnetName;"--address-prefixes";addressPrefixes]
+        azArr <| subnetCreationArguments "update" rg name vnetName addressPrefixes
 
-    let create rg name vnetName addressPrefixes  = 
-        azArr ["network";"vnet";"subnet";"create";"-g";rg;"-n";name;"--vnet-name";vnetName;"--address-prefixes";addressPrefixes]
+    let create rg name vnetName addressPrefixes = 
+        azArr <| subnetCreationArguments "create" rg name vnetName addressPrefixes
 
-    let createOrUpdate rg name vnetName addressPrefixes =
+    let createOrUpdate op rg name vnetName addressPrefixes delegations =
+        subnetCreationArguments op rg name vnetName addressPrefixes @ ["--delegations";delegations]
+        |> azArr
+
+    let createOrUpdateForWeb rg name vnetName addressPrefixes =
         let result = show rg name vnetName
 
-        match result.ExitCode with
-        | 0 -> update rg name vnetName addressPrefixes
-        | _ -> create rg name vnetName addressPrefixes
- 
+        let op = match result.ExitCode with
+                    | 0 -> update rg name vnetName addressPrefixes
+                    | _ -> create rg name vnetName addressPrefixes
+        createOrUpdate op rg name vnetName addressPrefixes "Microsoft.Web/serverfarms"
+
     let resourceId rg vnetName name =
         Common.resourceId rg "virtualNetworks" vnetName + (sprintf "/subnets/%s" name)
