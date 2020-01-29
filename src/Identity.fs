b614e042bef6339315ae367c4dac17e4d8200202
Olav Nybo <olavnybo@gmail.com>
2020-01-29 15:41:58 +0100

    change return value of create identity to typed record rather than string

--- src/Identity.fs
+++ src/Identity.fs
@@ -9,21 +9,6 @@ module Identity =
     type LocationType = {
         location: string
     }
-    let create rg name =
-        let url = 
-            sprintf "resourceGroups/%s/providers/Microsoft.ManagedIdentity/userAssignedIdentities/%s?api-version=2015-08-31-preview" rg name
-
-        let accessTokenResult = getAccessToken "https://management.azure.com"
-
-        let result = put Management url (Some accessTokenResult.accessToken) (Some (box {location = defaultLocation}))
-        match (result) with
-               |(HttpStatusCode.OK, value) -> 
-                    printfn "Updated managed identity (user assigned)"
-                    value
-               |(HttpStatusCode.Created, value) ->
-                    printfn "Created managed identity (user assigned)"
-                    value
-               |(statusCode, value) -> failwithf "Could not create managed identity (user assigned). Status code is %A. Content: %s" statusCode value
 
     type Tag = {
         key: string
@@ -35,7 +20,7 @@ module Identity =
         principalId: string
         tenantId: string
     }
-    type IdentityResponse = {
+    type ManagedIdentity = {
         id: string
         location: string
         name: string
@@ -45,17 +30,44 @@ module Identity =
         _type: string
     }
 
+    let create rg name =
+        let url = 
+            sprintf "resourceGroups/%s/providers/Microsoft.ManagedIdentity/userAssignedIdentities/%s?api-version=2015-08-31-preview" rg name
+
+        let accessTokenResult = getAccessToken "https://management.azure.com"
+
+        let result = put Management url (Some accessTokenResult.accessToken) (Some (box {location = defaultLocation}))
+        match (result) with
+               |(HttpStatusCode.OK, value) -> 
+                    printfn "Updated managed identity (user assigned)"
+                    JsonConvert.DeserializeObject<ManagedIdentity> value
+               |(HttpStatusCode.Created, value) ->
+                    printfn "Created managed identity (user assigned)"
+                    JsonConvert.DeserializeObject<ManagedIdentity> value
+               |(statusCode, value) -> failwithf "Could not create managed identity (user assigned). Status code is %A. Content: %s" statusCode value
+
     let get rg name =
         let url = 
             sprintf "resourceGroups/%s/providers/Microsoft.ManagedIdentity/userAssignedIdentities/%s?api-version=2018-11-30" rg name
 
         let accessTokenResult = getAccessToken "https://management.azure.com"
 
-        let result = get<IdentityResponse> Management url (Some accessTokenResult.accessToken)
+        let result = get<ManagedIdentity> Management url (Some accessTokenResult.accessToken)
         match (result) with
                |(HttpStatusCode.OK, value) -> 
                     value
                |(statusCode, value) -> failwithf "Could not get managed identity (user assigned). Status code is %A. Content: %A" statusCode value
 
     let resourceId rg name = 
-        (Group.rgId rg) + (sprintf "/providers/Microsoft.ManagedIdentity/userAssignedIdentities/%s" name)    
+        (Group.rgId rg) + (sprintf "/providers/Microsoft.ManagedIdentity/userAssignedIdentities/%s" name)
+
+    let ensureServicePrincipalAvailable principalId =
+        let mutable counter = 0
+        System.Threading.Thread.Sleep 10000
+        while (not (Ad.ServicePrincipal.exist principalId)) do
+            counter <- counter + 1
+            printfn "not yet found service principal"
+            if counter = 10 then failwithf "serviceprincipal %s was not found after %d tries" principalId counter
+            System.Threading.Thread.Sleep 10000
+        printfn "service principal %s found" principalId
+        ()
